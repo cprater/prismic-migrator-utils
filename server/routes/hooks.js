23e5abf771f75223f01ba1b62ba812b62f33f6a3
Chris Prater <>
2026-04-27 13:27:41 -0600

    more logging

--- server/routes/hooks.js
+++ server/routes/hooks.js
@@ -5,6 +5,7 @@ const router = Router();
 
 
 router.post("/publish", async (req, res) => {
+  console.log('PUBLISH HOOK CALLED WITH HEADERS', req.headers);
   console.log('PUBLISH CALLED WITH BODY', req.body)
 
   return res.status(200).json({ reason: "hook received" });
